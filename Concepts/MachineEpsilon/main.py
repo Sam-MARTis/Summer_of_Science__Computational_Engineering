d26c9921678e0b81829b620d41edee5ff3f9c64a
Samanth Martis <samanthmartis@gmail.com>
2024-05-25 22:28:59 +0530

    Added explanation

--- Concepts/MachineEpsilon/main.py
+++ Concepts/MachineEpsilon/main.py
@@ -1,5 +1,16 @@
 import math
 
+"""
+    This program calculates the machine epsilon for a 64 bit machine
+    A machine epsilon is the smallest positive number that can be added to 1 and still have the result be greater than 1
+    Any postive number smaller than the machine epsilon that's added to 1 will result in the sum be treated as 1.
+    This is due to the finite precision of floating point numbers in computers.
+    1+number_less_than_epsilon is identical to 1 in the computer's memory and ALU.
+    
+"""
+
+
+
 def main():
     espsilon = 1
     while(1+espsilon>1):
